c609e6dabce915a8d75e1bf20d8aebcd1b0fef7f
okyereyawpatrick1-stack <okyereyawpatrick1@gmail.com>
2026-08-22 01:18:37 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-const CACHE_NAME = "oyp-v3";
+const CACHE_NAME = "oyp-v5";
 const ASSETS = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/store.js",
-  "/js/config.local.js",
-  "/js/africastalking.js",
-  "/js/bmsafrica.js",
-  "/js/hellio.js",
-  "/js/arkesel.js",
-  "/js/app.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/logo.jpeg"
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/store.js",
+  "./js/config.local.js",
+  "./js/africastalking.js",
+  "./js/bmsafrica.js",
+  "./js/hellio.js",
+  "./js/arkesel.js",
+  "./js/app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./logo.jpeg"
 ];
 
 self.addEventListener("install", function (e) {
